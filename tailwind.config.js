48fcb0111b46068f6baa0e0530156ea3eef958ab
Alfiya Shaikh <alfiyashaikh2506@ggit config --global user.email alfiyashaikh2506@gmail.com>
2024-03-14 08:43:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        notQuiteBlack: "#23272a",
+        blurple: "#5865f2",
+        black: "#060a0b",
+        brand: "#404eed",
+        white: "white",
+        darkCharcoal: "#2c2f33",
+        darkButtonHover: "#3b3b3b",
+        greyple: "#99aab5",
+        springGreen: "#57f287",
+        dimGrey: "#50555f",
+        offWhite: "#f6f6f6",
+        darkBlurple: "#3442d9",
+        fuchsia: "#eb459e",
+        yellow: "#fee75c",
+        mintGreen: "#43b581",
+        red: "#ed4245",
+        ekkoRed: "#de2761",
+        vividCerulean: "#00b0f4",
+        orange: "#fda220",
+        pink: "#ffcdee",
+        buttonHover: "#8891f2",
+        pinc2: "#dc4195",
+        green: "#52f180",
+        purple: "#808aff",
+        navyBlue: "#24173f",
+    },
+    },
+  },
+  plugins: [],
+}
+
